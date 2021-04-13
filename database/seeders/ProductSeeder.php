bfe62c8fa635dff904486fc79c7b9d62e8a06fa7
QueenAzhar <190103063@stu.sdu.edu.kz>
2021-04-13 17:23:35 +0300

    migrated product table

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('products')->insert([
+            [
+                'name'=>'Samsung Galaxy Buds plus Black',
+                "price"=>"24890 ₸",
+                "description"=>"Included with the headphones is a USB Type-C charging cable",
+                "category"=>"headphones",
+                "gallery"=>"https://alser.kz/products/naushniki-dlja-smartfonov//9XWldtqqN/nausniki-besprovodnye-samsung-galaxy-buds-plus-black-1040x650.jpg"
+            ],
+            [
+                'name'=>'Apple Watch',
+                "price"=>"231 900 ₸",
+                "description"=>"A smart watch with much more feature",
+                "category"=>"watch",
+                "gallery"=>"https://cdn-kaspi.kz/shop/medias/sys_master/images/images/hd1/h9f/26632957427742/apple-watch-series-6-44mm-space-gray-aluminium-case-with-sport-band-m00h3gk-a-cernyj-100568498-1.png"
+            ],
+            [
+                'name'=>'CANON Camera',
+                "price"=>"282 990 ₸",
+                "description"=>"A  camera with much more feature",
+                "category"=>"camera",
+                "gallery"=>"https://asset.conrad.com/media10/isa/160267/c1/-/en/1674385_LB_00_FB/image.jpg"
+            ],
+            [
+                'name'=>'Apple iPhone 12',
+                "price"=>"419 000 ₸",
+                "description"=>"A phone  with much more feature",
+                "category"=>"mobile phone",
+                "gallery"=>"https://kaz-shop.net/_sh/2000/200029.jpg"
+             ]
+        ]);
+    }
+}
